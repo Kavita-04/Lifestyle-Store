47848c2bf6b387f1b2bda4bfc0b1bc245a8e5ab9
Kavita-04 <Kavita.mcs20.du@gmail.com>
2021-08-15 15:39:50 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="container">
+        <center>
+            <p>Copyright &copy; Lifestyle Store. All Rights Reserved  |  Contact Us: +91 91234 56789</p>	
+        </center>
+    </div>
+</footer>
